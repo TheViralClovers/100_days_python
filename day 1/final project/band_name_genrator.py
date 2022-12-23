862b2969bee003094977fd909f4b3250297bc48a
TheViralClovers <theviralclovers@gmail.com>
2022-12-23 22:22:19 +0530

    day 1

---
+++ day 1/final project/band_name_genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city_name = input("Enter the name fo the city you grew up in: ")
+pet_name = input("Enter the name of the pet you had in your childhood: ")
+print(f"Your band name could be {city_name} {pet_name}")
