a84173639c250210f7db2faefdea0958802537f1
TheViralClovers <theviralclovers@gmail.com>
2022-12-25 23:08:31 +0530

    modularised code

--- day 7/final project/hangman.py
+++ day 7/final project/hangman.py
@@ -1,73 +1,7 @@
 from random import choice
 import os
-
-HANGMANPICS = ['''
-
-  _                                             
- | |                                            
- | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
- | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
- | | | | (_| | | | | (_| | | | | | | (_| | | | |
- |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                     __/ |                      
-                    |___/                       ''','''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-
-words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
-         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
-         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
-         'otter owl panda parrot pigeon python rabbit ram rat raven '
-         'rhino salmon seal shark sheep skunk sloth snake spider '
-         'stork swan tiger toad trout turkey turtle weasel whale wolf '
-         'wombat zebra ').split()
+from hangman_art import HANGMANPICS
+from hangman_words import words
 
 def start():
 	lives = 7
@@ -115,6 +49,6 @@ while True:
 	if(not lives):
 		print_game(lives)
 		print("\nYou lose!")
-	if(input("Do you want to continue? yes or no ")!="yes"):
+	if(input("Do you want to continue? yes or no: ")!="yes"):
 		print("Thank you for playing!")
 		break
